bd85a1e6316ea37095c332ffa55610285a4104a3
Emmily Kettily Bezerra da Silva <20231101110041@ifrn.local>
2026-01-29 10:48:28 -0300

    :lipstick: style: CSS do home melhorado

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,6 +9,37 @@ function Home() {
       subtitle="Explore uma seleção de produtos importados que trazem qualidade, inovação e estilo ao seu dia a dia"
       buttonText="Ver projetos"
       image="/images/hero/hero-home.png"/>
+
+      <div className="benefits-section">
+        <div className="benefit-card">
+          <p>Até 24x<br />sem juros</p>
+        </div>
+  
+        <div className="benefit-card">
+          <p>Rewards</p>
+        </div>
+  
+        <div className="benefit-card">
+          <p>Frete grátis</p>
+        </div>
+  
+        <div className="benefit-card">
+          <p>Produtos<br />exclusivos</p>
+        </div>
+      </div>
+
+      <footer className="footer">
+        <p>
+          Copyright© 1995-2026 MICEAimports. Todos os direitos reservados.<br />
+          MICEAimports Eletrônica da Amazônia LTDA., com sede em Av. dos Oitis, nº 1.460, Distrito Industrial, Manaus/AM, 69.007-002, inscrita no CNPJ/MF sob o nº. 00.280.273/0001-37.
+        </p>
+        <p>
+          Esse website é melhor visualizado nas versões Microsoft Internet Explorer 11 ou superior e/ou nas últimas versões dos navegadores Google Chrome e Mozilla Firefox.
+        </p>
+        <p>
+          Formas de pagamento aceitas: Pix, Cartão de crédito (Visa, MasterCard, Elo, American Express, Diners Club, Hipercard, Elo) e Cartão Samsung Itaú.
+        </p>
+      </footer>
     </div>
   );
 }
